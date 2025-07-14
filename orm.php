0b0daf0c94b1023a1a0228f44d5c4b834a5e51d5
Tanju Yıldız <ben@tanjuyildiz.com>
2025-07-14 20:28:56 +0300

    🧩 Yapısal Geliştirme

--- orm.php
+++ orm.php
@@ -3,9 +3,72 @@ class Orm {
     protected $db;
     protected $tablo;
 
+    protected $wheres = [];
+    protected $params = [];
+    protected $order = '';
+    protected $limit = '';
+    protected $offset = '';
+
     public function __construct($tablo) {
         $this->db = Veritabani::baglan();
         $this->tablo = $tablo;
+    }
+
+    public function where($kolon, $islem, $deger) {
+        $paramKey = ':w_' . count($this->params);
+        $this->wheres[] = "$kolon $islem $paramKey";
+        $this->params[$paramKey] = $deger;
+        return $this;
+    }
+
+    public function orWhere($kolon, $islem, $deger) {
+        $paramKey = ':w_' . count($this->params);
+        if (empty($this->wheres)) {
+            $this->wheres[] = "$kolon $islem $paramKey";
+        } else {
+            $this->wheres[] = "OR $kolon $islem $paramKey";
+        }
+        $this->params[$paramKey] = $deger;
+        return $this;
+    }
+
+    public function orderBy($kolon, $yon = 'ASC') {
+        $this->order = " ORDER BY $kolon $yon";
+        return $this;
+    }
+
+    public function limit($adet, $baslangic = null) {
+        $this->limit = " LIMIT " . ($baslangic !== null ? "$baslangic, " : "") . "$adet";
+        return $this;
+    }
+
+    public function get() {
+        $sql = "SELECT * FROM {$this->tablo}";
+        if (!empty($this->wheres)) {
+            $sql .= ' WHERE ' . implode(' ', $this->wheres);
+        }
+        $sql .= $this->order;
+        $sql .= $this->limit;
+
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute($this->params);
+
+        $this->resetQuery(); // bir sonraki sorgu için temizlik
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function first() {
+        $this->limit(1);
+        $sonuc = $this->get();
+        return $sonuc[0] ?? null;
+    }
+
+    public function resetQuery() {
+        $this->wheres = [];
+        $this->params = [];
+        $this->order = '';
+        $this->limit = '';
+        $this->offset = '';
     }
 
     public function find($id) {
